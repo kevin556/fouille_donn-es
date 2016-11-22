c61ee000b7a7b1706d95d6c1053f266fa3b26b5f
POGORELOV mikhail <pogorelo@informatique.univ-paris-diderot.fr>
2016-11-22 23:33:29 +0100

    Updated insert method

--- dev/insert_db.py
+++ dev/insert_db.py
@@ -10,8 +10,15 @@ db= client.movie_db
 if(db.movie_db.count() > 0):
 	db.movie_db.delete_many({})
 
-tmp = get_data()
+tmp = get_data(3)
+
+print tmp[0]
 for i in tmp:
-	db.movie_db.insert(json.loads(i))
+	# db.movie_db.insert(json.loads(i))
+	result = db.movie_db.insert_one(
+		json.loads(tmp[0]))
+	print result
 print db.movie_db.count()
+	
+
 	
