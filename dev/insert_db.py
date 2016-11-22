21aac0d2dbd3dd68933b8e61ca7734ca030af436
Kevin Youna <kevin.youna@gmail.com>
2016-11-22 22:37:29 +0100

    version finale de insert_db.py

--- dev/insert_db.py
+++ dev/insert_db.py
@@ -3,16 +3,15 @@ from pymongo import MongoClient
 from pie import get_data
 import json
 
-def init_db:
-	tmp =()
-	client = MongoClient()
+tmp =()
+client = MongoClient()
 
-	db= client.movie_db
-	if(db.movie_db.count() > 0):
-		db.movie_db.delete_many({})
+db= client.movie_db
+if(db.movie_db.count() > 0):
+	db.movie_db.delete_many({})
 
-	tmp = get_data()
-	for i in tmp:
-		db.movie_db.insert(json.loads(i))
-	print db.movie_db.count()
+tmp = get_data()
+for i in tmp:
+	db.movie_db.insert(json.loads(i))
+print db.movie_db.count()
 	
