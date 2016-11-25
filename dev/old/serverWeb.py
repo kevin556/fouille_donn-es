069595ded319bb92b427b23fd7c58280aa1ef405
POGORELOV mikhail <pogorelo@informatique.univ-paris-diderot.fr>
2016-11-25 16:30:04 +0100

    Put server code in 'old' directory

---
+++ dev/old/serverWeb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+ 
+import BaseHTTPServer
+import CGIHTTPServer
+ 
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", 8000)
+handler.cgi_directories = [""]
+ 
+httpd = server(server_address, handler)
+httpd.serve_forever()
+
+ # AddHandler cgi-script .cgi .py .pl
--- dev/v.py
+++
@@ -1,5 +0,0 @@
-def virus():
-	for x in xrange(1,10000):
-		print("VIRUS")
-	
-virus()
