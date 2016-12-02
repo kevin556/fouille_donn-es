e966e9bae7f276f4e94b932bbcc56168190bc5f9
Kevin Youna <kevin.youna@gmail.com>
2016-12-02 16:40:19 +0100

    ajout de fonction de gestion du contenu et ajout de la fonction permettant l' effacement des erreurs dans la base de données movies,il reste a implementer les changements dans la base movies

--- dev/user_fonc.py
+++ dev/user_fonc.py
@@ -6,16 +6,40 @@ import random
 import json
 
 nom_base_final = "user_info"
+nom_base_movie = "movie_db"
 
 
 #retourne le nombre de films dans la base.
 def get_nb_max_movie():
 	client = MongoClient()
-	db = client.movie_db
-	nb = db.movie_db.count()
+	db = client[nom_base_movie]
+	nb = db[nom_base_movie].count()
 	client.close()
 	return nb
+#donne le nombre d'utilisateurs.
+def get_nb_users():
+	client = MongoClient()
+	db = client[nom_base_final]
+	nb_user = db[nom_base_final].count()
+	print nb_user
+	return nb_user
+	client.close()
 
+
+#affiche tous les utilisateurs
+def show_users():
+	client = MongoClient()
+	db = client.user_info
+	print db.user_info.find({})
+	client.close()
+	
+#affiche un utilisateur en particulier
+def show_user(id_user):
+	client = MongoClient()
+	db = client.user_info
+	commande = '{ "id": "'+str(id_user) +'" }'
+	print commande
+	print db.user_info.find_one(commande)
 
 #retourne le nombre d'utilisateurs dans la table user_id
 def get_max_id():
@@ -39,56 +63,54 @@ def erase_users():
 	client.close()
 
 #efface un utilisateur grace a son id.
-'''
 def erase_user(id_user):
 	client = MongoClient()
 	db = client.user_info
 	commande={}
-	commande = '{"id " : ' + '"' + str(id_user) + '"}'
+	commande = '{ "id": "'+str(id_user) +'" } ' 
 	print commande
-	db.user_info.delete_many(commande)
+	db.user_info.remove(commande)
 	client.close()
-'''
 
 '''genere des listes de film et ajoute l'id de la personne qui 
 	a like ou dislike dans la base au niveau de la ligne du film
 '''
 
-def generate_liste_movie_liked(movie_nb,mode,id_user):
+#def modifie_base_movie():
+
+def generate_favorite_genre():
+	random.seed()
+	client = MongoClient()
+	db = client[nom_base_movie]
+	nb_film = db.movie_db.count()
+	print nb_film
+	commande = '{ "id": "'+str(random.randrange(nb_film)) +'" } '
+	print commande 
+	genre = db.movie_db.find_one(commande)
+	client.close()
+	print "genre %s "%(genre)
+	return genre
+
+def generate_liste_movie_liked(movie_nb,id_user):
 	random.seed()
 	client = MongoClient()
 	passed = False
 	liste =[]
+	liste_looked=[]
 	db = client.movie_db
 	nb_film = db.movie_db.count()
 	for i in range(0,movie_nb,1):
 		liste.append(random.randrange(0,nb_film,1))
-	print liste
-	print mode
+	for i in range(0,len(liste)-random.randrange(0,nb_film,1),1):
+		liste_looked.append(liste[i])
+	return liste,liste_looked
 
-	if(str(mode) == str(1)):
-		commande = 'like' +"':"  + str(id_user)+"'"
-		passed = True
-	if(str(mode) == str(-1)):
-		commande = "'dislike' : " + str(id_user) 
-		passed = True
-	if not passed :
-		print "erreur deuxieme argument"
-		return 
-
-	for i in liste:
-		db.movie_db.update({
-			'id':str(liste)
-			},{
-				'$set':{
-					commande
-				}
-			}
-			,upsert=False)
-
+def generate_rating(taille_liste,note_min,note_max):
+	random.seed()
+	liste =[]
+	for i in range(0,taille_liste,1):
+		liste.append(random.randrange(note_min,note_max))
 	return liste
-
-
 
 #genere les données random pour les utilisateurs 
 def random_data(movie_nb):
@@ -103,19 +125,42 @@ def random_data(movie_nb):
 	result['id']=max_id
 	result['username']="user_"+str(max_id)
 	result['ville']=liste_ville[b]
+	result['genre']=generate_favorite_genre()
 	#sexe -> random sur Homme ou Femme
 	result['sexe']=liste_sexe[c]
 	#age -> random sur un range d'age
 	result['age']=random.randrange(13,120,1)
-	result["like"]=generate_liste_movie_liked(movie_nb,"1",max_id)
-	result["dislike"]=generate_liste_movie_liked(movie_nb,"-1",max_id)
+	result["liked"], result["looked"] =generate_liste_movie_liked(movie_nb,max_id)
+	result["rating"] = generate_rating(len(result["liked"]),0,10);
 	return result
+
+def get_max_genre():
+	client = MongoClient()
+	maximum=0
+	genre =""
+	db = client["movie_db"]
+	src = db.movie_db.find({})
+	tab ={}
+	tmp = ""
+	for j in range(1,1000,1):
+		tmp ="'" + src.__getitem__(j)['Genre'] + "'"
+		if tmp not in tab:
+			tab[tmp]=1
+		else:
+			tab[tmp]+=1
+	for j in tab:
+		print tab[j]
+		maximum = max(int(tab[j]),maximum)
+		if(maximum == int(tab[j])):
+			genre = j
+	return tab,genre
+	'''return tab,maximum'''
+	
 
 
 #cree les utilisateurs 
 def create_user(nb_user):
 	client = MongoClient()
-
 	db=client.user_info
 	for i in range(0,nb_user,1):
 		a = random_data(100)
@@ -123,7 +168,4 @@ def create_user(nb_user):
 
 	client.close()
 
-create_user(1)
-
-'''erase_user(1)'''
 
