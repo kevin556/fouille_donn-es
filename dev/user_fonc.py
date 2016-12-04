6ed0e05f07f7724e96bd772519e3b11b38f7bd6c
Kevin Youna <kevin.youna@gmail.com>
2016-12-04 01:45:01 +0100

    ajout des derniers elements avant la mise en place du modele

--- dev/user_fonc.py
+++ dev/user_fonc.py
@@ -3,12 +3,15 @@
 '''a faire dans un shell mongodb avant de lancer les fonctions
 	db.movie_db.find().forEach( function(e){ e.Rating = new Array() ;db.movie_db.save(e); });
 	db.movie_db.find().forEach( function(e){ e.Looked = new Array() ;db.movie_db.save(e); });
-
+	db.movie_db.find().forEach( function(e){ e.Looked = new Array() ;db.movie_db.save(e); });
+	
 '''
 
 from pymongo import MongoClient
 import random
 import json
+import operator
+
 
 nom_base_final = "user_info"
 nom_base_movie = "movie_db"
@@ -26,7 +29,7 @@ def get_nb_users():
 	client = MongoClient()
 	db = client[nom_base_final]
 	nb_user = db[nom_base_final].count()
-	print nb_user
+	
 	return nb_user
 	client.close()
 
@@ -57,7 +60,12 @@ def get_max_id():
 		return max_id
 
 
-#efface tous les utilisateurs
+'''
+	efface tous les utilisateurs
+	n'a pas encore été testée.
+	
+'''
+
 def erase_users():
 	client = MongoClient()
 	db = client.user_info
@@ -77,7 +85,10 @@ def erase_users():
 	'''
 	client.close()
 
-#efface un utilisateur grace a son id.
+'''
+	Efface un utilisateur grace a son id,ainsi que sa présence dans la liste looked de chaque films
+	
+'''
 def erase_user(id_user):
 	client = MongoClient()
 	db = client.user_info
@@ -93,8 +104,8 @@ def erase_user(id_user):
 	db.user_info.remove({'id':str(id_user)})
 	client.close()
 
-'''genere des listes de film et ajoute l'id de la personne qui 
-	a like ou dislike dans la base au niveau de la ligne du film
+'''
+	renvoie la liste des notes du film avec l'id id_movie
 '''
 
 def get_current_rating(id_movie):
@@ -102,24 +113,23 @@ def get_current_rating(id_movie):
 	db = client.movie_db
 	'''il faut apparement rajouter une colonne rating dans la base films'''
 	note = db.movie_db.find_one({'_id':id_movie})['Rating']
-	print note
 	client.close()
 	return note
+
+'''
+	renvoie la liste des utilisateurs qui ont regardé le film id_movie
+'''
 
 def get_current_looked_list(id_movie):
 	client = MongoClient()
 	db = client.movie_db
 	note = db.movie_db.find_one({'_id':id_movie})["Looked"]
-	print note
 	client.close()
 	return note
 
 def modifie_base_movie(mode,liste_movie,liste_rating,id_user):
 	client = MongoClient()
 	db = client.movie_db
-	'''
-	data = db.movie_db.find({})
-	'''
 	if(mode == "liked"):
 		for i in range(0,len(liste_movie),1):
 			try:
@@ -130,11 +140,6 @@ def modifie_base_movie(mode,liste_movie,liste_rating,id_user):
 				     'Rating': liste_rating[i]
 				  }
 				}, upsert=False)
-				'''
-				print tmp.modified_count
-				print tmp.matched_count
-				print tmp.raw_result
-				'''
 			except Exception:
 				print "add_to_rating %s"%(Exception)
 	if(mode == "looked"):
@@ -147,22 +152,17 @@ def modifie_base_movie(mode,liste_movie,liste_rating,id_user):
 				     'Looked':id_user
 				  }
 				}, upsert=False)
-				'''
-				print tmp.modified_count
-				print tmp.matched_count
-				print tmp.raw_result
-				'''
 			except Exception as ex:
 				template = "An exception of type {0} occured. Arguments:\n{1!r}"
 				message = template.format(type(ex).__name__, ex.args)
 				print message
-	'''
-	get_current_rating(liste_movie[i])
-	get_current_looked_list(liste_movie[i])
-	'''
 	client.close()
 
 
+'''
+	fonction qui va génerer une liste de films que l'utilisateur a regardé et "liké"
+	
+'''
 
 def generate_liste_movie_liked(movie_nb,id_user,min_like):
 	random.seed()
@@ -181,9 +181,12 @@ def generate_liste_movie_liked(movie_nb,id_user,min_like):
 	for i in range(0,hasard,1):
 		liste_looked.append(liste[i])
 	client.close()
-	print liste_looked,liste
 	return liste_looked,liste
 
+'''
+	fonction qui va generer des notes entre note_min et note_max
+
+'''
 def generate_rating(taille_liste,note_min,note_max):
 	random.seed()
 	liste =[]
@@ -191,40 +194,10 @@ def generate_rating(taille_liste,note_min,note_max):
 		liste.append(random.randrange(note_min,note_max))
 	return liste
 
-#genere les données random pour les utilisateurs 
-def random_data(movie_nb,min_like):
-	nb_champ_genre = 3
-	nb_champ_country = 2
-	nb_champ_year = 2
-	nb_champ_language = 2
-
-	random.seed()
-	liste_ville= ["paris","lyon","toulouse","marseille","nice","nantes","strasbourg"];
-	liste_sexe=["Homme","Femme"]
-	max_id = get_max_id()
-	max_movie_nb = get_nb_max_movie()
-	b = random.randrange(0,len(liste_ville),1)
-	c = random.randrange(0,len(liste_sexe),1)
-	result ={}
-	result['id']=max_id
-	result['username']="user_"+str(max_id)
-	result['ville']=liste_ville[b]
-	#sexe -> random sur Homme ou Femme
-	result['sexe']=liste_sexe[c]
-	#age -> random sur un range d'age
-	result['age']=random.randrange(13,120,1)
-	result["liked"], result["looked"] =generate_liste_movie_liked(movie_nb,max_id,min_like)
-	result["rating"] = generate_rating(len(result["liked"]),0,10);
-	
-	result['genre']=generate_favorite_(nb_champ_genre,movie_nb,'Genre')
-	result['country']=generate_favorite_(nb_champ_country,movie_nb,'Country')
-	result['year']=generate_favorite_(nb_champ_year,movie_nb,'Year')
-	result['language']=generate_favorite_(nb_champ_language,movie_nb,'Language')
-	
-
-	modifie_base_movie("liked",result["liked"],result["rating"],result["id"])
-	modifie_base_movie("looked",result["liked"],result["rating"],result["id"])
-	return result
+'''
+	fonction qui va aller chercher dans les films des utilisateurs un nombre max_champ d'elements
+	par rapport a un critere precis et renvoie une liste random avec ces éléments.
+'''
 
 def generate_favorite_(nb_max_champ,nb_film,critere):
 	random.seed()
@@ -243,8 +216,74 @@ def generate_favorite_(nb_max_champ,nb_film,critere):
 		i+=1	
 
 	client.close()
-	print "%s : %s "%(critere,genre)
+	
 	return genre
+
+'''
+	genere la liste des éléments majoritaires parmi les choix de l'utilisateur
+'''
+def get_best(nb_max_element,liste_film,critere):
+	client = MongoClient()
+	db = client.movie_db
+	tab = {}
+	liste = []
+	for i in liste_film:
+		tmp = db.movie_db.find_one({'_id':i})[critere]
+		if tmp not in tab:
+			tab[tmp]=1
+		else:
+			tab[tmp]+=1
+	for i in range(0,nb_max_element,1):
+		liste.append(sorted(tab,key = tab.get,reverse = True)[i])
+	return liste
+	client.close()
+
+#genere les données random pour les utilisateurs 
+def random_data(movie_nb,min_like):
+	nb_champ_genre = 3
+	nb_champ_country = 2
+	nb_champ_year = 2
+	nb_champ_language = 2
+	nb_search_element = 3
+	nb_best_element = 3
+	random.seed()
+	liste_ville= ["paris","lyon","toulouse","marseille","nice","nantes","strasbourg"];
+	liste_sexe=["Homme","Femme"]
+	max_id = get_max_id()
+	max_movie_nb = get_nb_max_movie()
+	b = random.randrange(0,len(liste_ville),1)
+	c = random.randrange(0,len(liste_sexe),1)
+	result ={}
+	result['id']=max_id
+	result['username']="user_"+str(max_id)
+	result['ville']=liste_ville[b]
+	#sexe -> random sur Homme ou Femme
+	result['sexe']=liste_sexe[c]
+	#age -> random sur un range d'age
+	result['age']=random.randrange(13,120,1)
+	result["liked"], result["looked"] =generate_liste_movie_liked(movie_nb,max_id,min_like)
+	result["rating"] = generate_rating(len(result["liked"]),0,10);
+	
+	result["best_genre"] = get_best(nb_best_element,result['looked'],'Genre')
+	result["best_language"]=get_best(nb_best_element,result['looked'],'Language')
+	result["best_year"]=get_best(nb_best_element,result['looked'],'Year')
+	result["best_actor"]=get_best(nb_best_element,result['looked'],'Genre')
+
+	result["search_actor"]=generate_favorite_(nb_search_element,movie_nb,'Actors')
+	result["search_genre"]=generate_favorite_(nb_search_element,movie_nb,'Genre')
+	result["search_year"]=generate_favorite_(nb_search_element,movie_nb,'Year')
+	result["search_country"]=generate_favorite_(nb_search_element,movie_nb,'Country')
+	result["search_language"]=generate_favorite_(nb_search_element,movie_nb,'Language')
+
+	result['genre']=generate_favorite_(nb_champ_genre,movie_nb,'Genre')
+	result['country']=generate_favorite_(nb_champ_country,movie_nb,'Country')
+	result['year']=generate_favorite_(nb_champ_year,movie_nb,'Year')
+	result['language']=generate_favorite_(nb_champ_language,movie_nb,'Language')
+
+	modifie_base_movie("liked",result["liked"],result["rating"],result["id"])
+	modifie_base_movie("looked",result["liked"],result["rating"],result["id"])
+	return result
+
 
 
 #cree les utilisateurs 
